71b32f751bf3168ecf31af16eca0b5f48e1f9a32
Soichiro Isshiki <sisshiki@mac.com>
2022-11-12 17:29:54 +0900

    Add tests.

--- times.sh
+++ times.sh
@@ -1,2 +1,2 @@
 cargo build --release
-benchmark-driver benchmark/integer_times.yaml --rbenv '3.2.0-dev; 3.2.0-dev --yjit' -e 'target/release/monoruby' -e 'target/release/monoruby --no-jit' --repeat-count 3
+benchmark-driver benchmark/integer_times.yaml benchmark/vm_block.yml benchmark/vm_yield.yml --rbenv '3.2.0-dev; 3.2.0-dev --yjit' -e 'target/release/monoruby' -e 'target/release/monoruby --no-jit' --repeat-count 3
