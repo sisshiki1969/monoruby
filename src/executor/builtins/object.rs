21f24438cbbd3df09018a10b95326bc1122b3337
Soichiro Isshiki <sisshiki@mac.com>
2023-01-01 19:46:14 +0900

    increase coverage.

--- src/executor/builtins/object.rs
+++ src/executor/builtins/object.rs
@@ -417,6 +417,26 @@ mod test {
         run_test2(
             r#"a=Object.new; a.instance_variable_set("@i", 42); a.instance_variable_get(:@i)"#,
         );
+        run_test2(
+            r#"
+            20000.times do
+                a=Object.new;
+                a.instance_variable_set("@a", 0);
+                a.instance_variable_set("@b", 1);
+                a.instance_variable_set("@c", 2);
+                a.instance_variable_set("@d", 3);
+                a.instance_variable_set("@e", 4);
+                a.instance_variable_set("@f", 5);
+                a.instance_variable_set("@g", 6);
+                a.instance_variable_set("@h", 7);
+                a.instance_variable_set("@i", 8);
+                a.instance_variable_set("@j", 9);
+                a.instance_variable_set("@k", 10);
+                a.instance_variable_set("@l", 11);
+                a.inspect;
+            end
+            "#,
+        );
     }
 
     #[test]
