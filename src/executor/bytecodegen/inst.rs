88aec9b0660aac855b0e0c85466f42b980674312
Soichiro Isshiki <sisshiki@mac.com>
2023-05-17 15:12:28 +0900

    Fix.

--- src/executor/bytecodegen/inst.rs
+++ src/executor/bytecodegen/inst.rs
@@ -1041,6 +1041,6 @@ impl TraceIr {
     pub(crate) fn is_terminal(pc: BcPc) -> bool {
         let op = pc.op1;
         let opcode = (op >> 48) as u16;
-        opcode == 3 || opcode == 80 || opcode == 81 || opcode == 82 // Br or Ret or MethodRet
+        opcode == 3 || opcode == 80 || opcode == 81 || opcode == 82 // Br or Ret or MethodRet or Break
     }
 }
