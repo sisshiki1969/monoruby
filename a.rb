05a08f032c2bae0f65d1d52b8db39dabc97907e5
Soichiro Isshiki <sisshiki@mac.com>
2023-08-18 08:22:30 +0900

    Add a test. (now it fails)

--- a.rb
+++ a.rb
@@ -1,6 +1,12 @@
-@peeks = {}
-peek = [1,2,3,4,5]
-peek = @peeks[peek] ||= peek
-#peek = @peeks[peek] || (@peeks[peek] = peek)
-#       %1 = %2.[%1]    %2 = @peeks; %2:.[%1:] = %1
-puts "peak:#{peek} @peeks:#{@peeks}"
+def f
+  sum = 100
+  if sum < 0
+    sum += 1
+    begin
+    end while false
+  end
+end
+
+20.times { f }
+
+puts f
