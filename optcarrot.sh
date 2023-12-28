a454f7f83a52d0c3259b2da04b65c2aa5f47374f
Soichiro Isshiki <sisshiki@mac.com>
2023-12-28 18:05:01 +0900

    Fix.

--- optcarrot.sh
+++ optcarrot.sh
@@ -1,19 +1,11 @@
 cargo install --path monoruby
-rbenv local 3.3.0-preview3
-ruby -v
-ruby ../optcarrot/bin/optcarrot -b ../optcarrot/examples/Lan_Master.nes
-ruby ../optcarrot/bin/optcarrot -b --opt ../optcarrot/examples/Lan_Master.nes
-echo
-
+rbenv local 3.3.0
 ruby -v --yjit
 ruby --yjit ../optcarrot/bin/optcarrot -b ../optcarrot/examples/Lan_Master.nes
 ruby --yjit ../optcarrot/bin/optcarrot -b --opt ../optcarrot/examples/Lan_Master.nes
 echo
 
 monoruby -v
-echo "with --no-jit"
-monoruby --no-jit ../optcarrot/bin/optcarrot -b ../optcarrot/examples/Lan_Master.nes
-monoruby --no-jit ../optcarrot/bin/optcarrot -b --opt ../optcarrot/examples/Lan_Master.nes
 echo "with jit"
 monoruby ../optcarrot/bin/optcarrot -b ../optcarrot/examples/Lan_Master.nes
 monoruby ../optcarrot/bin/optcarrot -b --opt ../optcarrot/examples/Lan_Master.nes
