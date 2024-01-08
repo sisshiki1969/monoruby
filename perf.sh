a120bfea82e09bfa7a4cd8f0af5e95bb214ebfb2
Soichiro Isshiki <sisshiki@mac.com>
2024-01-08 23:02:33 +0900

    Bug fix.

--- perf.sh
+++ perf.sh
@@ -1,5 +1,4 @@
-cargo install --features perf --path monoruby
-perf record -F 200 -g -- monoruby $@
+RUSTFLAGS=-Cforce-frame-pointers cargo install --features perf --path monoruby
+perf record -F 2000 -g -- monoruby $@
 perf script | ../FlameGraph/stackcollapse-perf.pl | ../FlameGraph/flamegraph.pl > out.svg
 rm perf.data*
-
