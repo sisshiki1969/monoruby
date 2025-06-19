f9fc873a097cd41b5e06f1f58ea022d13a919ea6
monochrome <sisshiki@mac.com>
2025-06-19 18:01:09 +0900

    Refactor.

--- indexmap/src/lib.rs
+++ indexmap/src/lib.rs
@@ -118,7 +118,34 @@ pub mod set;
 
 pub use crate::map::RubyMap;
 pub use crate::set::IndexSet;
-pub use equivalent::Equivalent;
+
+use core::borrow::Borrow;
+
+/// Key equivalence trait.
+///
+/// This trait allows hash table lookup to be customized. It has one blanket
+/// implementation that uses the regular solution with `Borrow` and `Eq`, just
+/// like `HashMap` does, so that you can pass `&str` to lookup into a map with
+/// `String` keys and so on.
+///
+/// # Contract
+///
+/// The implementor **must** hash like `K`, if it is hashable.
+pub trait Equivalent<K: ?Sized> {
+    /// Compare self to `key` and return `true` if they are equal.
+    fn equivalent(&self, key: &K) -> bool;
+}
+
+impl<Q: ?Sized, K: ?Sized> Equivalent<K> for Q
+where
+    Q: Eq,
+    K: Borrow<Q>,
+{
+    #[inline]
+    fn equivalent(&self, key: &K) -> bool {
+        PartialEq::eq(self, key.borrow())
+    }
+}
 
 // shared private items
 
